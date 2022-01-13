59e95c48e229a9a457aadabf456f089626eb8451
Burak-Tasci <buraaktascii@outlook.com>
2022-01-14 00:42:12 +0300

    Implemented added note repository methods from service interface

--- src/main/java/com/tsci/notes/service/NotesServiceImpl.java
+++ src/main/java/com/tsci/notes/service/NotesServiceImpl.java
@@ -1,12 +1,16 @@
 package com.tsci.notes.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tsci.notes.dao.NoteRepository;
 import com.tsci.notes.dao.UserRepository;
+import com.tsci.notes.exception.NoteNotFoundException;
 import com.tsci.notes.exception.UserNotFoundException;
+import com.tsci.notes.model.Note;
 import com.tsci.notes.model.User;
 
 @Service
@@ -14,6 +18,15 @@ public class NotesServiceImpl implements NotesService {
 
 	// Create User Repository object
 	private UserRepository userRepository;
+	// Create Note Repository object
+	private NoteRepository noteRepository;
+	
+	// Connect noteRepository with service
+	@Autowired
+	public void setNoteRepository(NoteRepository noteRepository) {
+		this.noteRepository = noteRepository;
+	}
+
 	// Connect userRepository with service
 	@Autowired
 	public void setUserRepository(UserRepository userRepository) {
@@ -42,6 +55,27 @@ public class NotesServiceImpl implements NotesService {
 		List<User> users = userRepository.findByName(name);
 		if(users == null) throw new UserNotFoundException(String.format("There are no user found with name %s", name));
 		else return users;
+	}
+
+	@Override
+	public List<Note> findNotes() throws NoteNotFoundException {
+		List<Note> notes = noteRepository.findNotes();
+		if(notes == null) throw new NoteNotFoundException(String.format("There are no note found!"));
+		else return notes;
+	}
+	
+	@Override
+	public List<Note> findNotesByUserID(int id) throws NoteNotFoundException {
+		List<Note> notes = noteRepository.findNotesByUserID(id);
+		if(notes == null) throw new NoteNotFoundException(String.format("There are no note found with user id %s", id));
+		else return notes;
+	}
+
+	@Override
+	public List<Note> findNotesAfterDate(Date date) throws NoteNotFoundException {
+		List<Note> notes = noteRepository.findNotesAfterDate(date);
+		if(notes == null) throw new NoteNotFoundException(String.format("There are no note found after date %s", date));
+		else return notes;
 	}
 
 }
