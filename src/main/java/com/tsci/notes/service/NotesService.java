2434b99defc497ff2e5bc8ce629ddd7888783f5f
Burak-Tasci <buraaktascii@outlook.com>
2022-01-14 00:41:05 +0300

    Add NoteRepository methods to service interface

--- src/main/java/com/tsci/notes/service/NotesService.java
+++ src/main/java/com/tsci/notes/service/NotesService.java
@@ -3,6 +3,7 @@ package com.tsci.notes.service;
 import java.sql.Date;
 import java.util.List;
 
+import com.tsci.notes.exception.NoteNotFoundException;
 import com.tsci.notes.exception.UserNotFoundException;
 import com.tsci.notes.model.Note;
 import com.tsci.notes.model.User;
@@ -15,7 +16,7 @@ public interface NotesService {
 	List<User> findByName(String name) throws UserNotFoundException;
 
 	// Note Repository methods
-	List<Note> findNotes();
-	List<Note> findNotesByUserID(int id);
-	List<Note> findNotesAfterDate(Date date);
+	List<Note> findNotes() throws NoteNotFoundException;
+	List<Note> findNotesByUserID(int id) throws NoteNotFoundException;
+	List<Note> findNotesAfterDate(Date date) throws NoteNotFoundException;
 }
