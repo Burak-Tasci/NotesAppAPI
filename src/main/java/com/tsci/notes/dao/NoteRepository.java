1cd881c5877edfe8ec53702468ad933fab27aa36
Burak-Tasci <buraaktascii@outlook.com>
2022-01-13 21:57:31 +0300

    Add methods to Note Repository

--- src/main/java/com/tsci/notes/dao/NoteRepository.java
+++ src/main/java/com/tsci/notes/dao/NoteRepository.java
@@ -1,5 +1,14 @@
 package com.tsci.notes.dao;
 
+import java.sql.Date;
+import java.util.List;
+
+import com.tsci.notes.model.Note;
+
 public interface NoteRepository {
+	
+	List<Note> findNotes();
+	List<Note> findNotesByID(int id);
+	List<Note> findNotesAfterDate(Date date);
 
 }
