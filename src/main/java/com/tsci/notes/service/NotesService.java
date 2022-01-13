f08941ef4fc0c316a58a84ccbffde94285be7a58
Burak-Tasci <buraaktascii@outlook.com>
2022-01-13 22:30:59 +0300

    Add NoteRepository methods

--- src/main/java/com/tsci/notes/service/NotesService.java
+++ src/main/java/com/tsci/notes/service/NotesService.java
@@ -1,14 +1,21 @@
 package com.tsci.notes.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.tsci.notes.exception.UserNotFoundException;
+import com.tsci.notes.model.Note;
 import com.tsci.notes.model.User;
 
 public interface NotesService {
 
+	// User Repository methods
 	List<User> findUsers() throws UserNotFoundException;
 	List<User> findByTitle(String title) throws UserNotFoundException;
 	List<User> findByName(String name) throws UserNotFoundException;
-	
+
+	// Note Repository methods
+	List<Note> findNotes();
+	List<Note> findNotesByUserID(int id);
+	List<Note> findNotesAfterDate(Date date);
 }
